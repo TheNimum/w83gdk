e27cf2d9e92603b9d6cb0b7e9d2fddf51fabda54
Mikael Pajari <mikael.pajari@live.se>
2020-09-17 13:02:36 +0200

    klar

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace w83gdk
+{
+    class Program
+    {
+        public enum Enum
+            {
+                Value1 = -100,
+                Value2,
+                Value3 = 100,
+                Value4,
+            }
+        static void Main(string[] args)
+        {
+            
+            Enum[] enums = {
+                Enum.Value1, Enum.Value2,
+                Enum.Value3, Enum.Value4
+            };
+            for (int i=0; i < enums.Length; ++i)
+            {
+                Console.WriteLine((int)enums[i]);
+            }
+        }
+    }
+}
